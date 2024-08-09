47ba09af4fe2056f724beb188293308e72ace4bb
clausvalca226 <50083224+clausvalca226@users.noreply.github.com>
2024-08-08 22:30:28 -0400

    Update flashstorage.ts

--- flashstorage.ts
+++ flashstorage.ts
@@ -1,38 +1,32 @@
 
 
 //% color="#B19200" 
-//% icon="\ue0b7"
+//% icon="\uf0e7"
 //% block="Flash Storage"
 namespace flashstorage {
 
     let simMap : any = null
 
-    //% blockId="flash pairs stored" block="flash pairs stored"
+    //% blockId="flash_pairs_stored" block="number of pairs stored in flash"
     //% shim=flashstorage::size
     export function size() : number {
         return simMap ? simMap.size : 0;
     }
 
-    //% blockId="flash get or default" block="flash get %key or use %defaultValue if key not saved"
-    export function getOrDefault(key: string, defaultValue: string): string {
-        let value = get(key)
-        return value.length == 0 ? defaultValue : value;
-    }
-
-    //% blockId="flash remove key" block="flash remove key %key"
+    //% blockId="flash_remove_key" block="remove %key pair"
     //% shim=flashstorage::remove
     export function remove(key: string) : void {
         if (simMap) simMap.delete(key);
     }
 
-    //% blockId="flash get key" block="flash get key %key"
+    //% blockId="flash_get_key" block="get value for %key pair"
     //% shim=flashstorage::get
     export function get(key: string) : string {
         return simMap ? simMap[key] : "";
     }
 
 
-    //% blockId="flash put value for key" block="flash key %key's value to %value"
+    //% blockId="flash_put_value" block="save %key : %value pair"
     //% shim=flashstorage::put
     export function put(key: string, value: string) {
         if (!simMap) simMap = {};
