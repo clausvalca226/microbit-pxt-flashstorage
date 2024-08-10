7eaf5fe8139def1a663854a9a84cd46f9f9dbc11
clausvalca226 <50083224+clausvalca226@users.noreply.github.com>
2024-08-10 16:41:11 -0400

    Update flashstorage.ts

--- flashstorage.ts
+++ flashstorage.ts
@@ -5,27 +5,37 @@
 //% block="Flash Storage"
 namespace flashstorage {
 
-    let simMap : any = new Map();
+    //let simMap : any = new Map();
+    var simMapKeys : string[] = [];
+    var simMapValues : string[] = [];
+    
 
     //% blockId="flash_pairs_stored" block="number of pairs stored in flash"
     //% shim=flashstorage::size
     export function size() : number {
         //if (!simMap) return 0;
-        return simMap.size;
+        return simMapKeys.length;
     }
 
     //% blockId="flash_remove_key" block="remove %key pair"
     //% shim=flashstorage::remove
     export function remove(key: string) : void {
         //if (simMap) simMap.delete(key);
-        simMap.delet(key);
+        //simMap.delet(key);
+        let i = simMapKeys.indexOf(key);
+        if(i<0) return;
+        simMapKeys.removeAt(i);
+        simMapValues.removeAt(i);
     }
 
     //% blockId="flash_get_key" block="get value for %key pair"
     //% shim=flashstorage::get
     export function get(key: string) : string {
         //return simMap ? simMap[key] : "";
-        return simMap.get(key);
+        //return simMap.get(key);
+        let i = simMapKeys.indexOf(key);
+        if(i<0) return "";
+        return simMapValues[i];
     }
 
 
@@ -34,7 +44,9 @@ namespace flashstorage {
     export function put(key: string, value: string) : number {
         //if (!simMap) simMap = {};
         //simMap[key] = value;
-        simMap.set(key,string);
+        //simMap.set(key,string);
+        simMapKeys.push(key);
+        simMapValues.push(value);
         return 0;
     }
 }
